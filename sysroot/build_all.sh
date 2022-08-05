1134c552bfcf42e4d021176bdc7c2062ad0d2150
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2022-08-04 17:21:44 -0700

    feat: add a script to build all sysroots in sequence
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

---
+++ sysroot/build_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Copyright (c) Joby Aviation 2022
+# Original authors: Thulio Ferraz Assis (thulio@aspect.dev), Aspect.dev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+repo_root=$(git rev-parse --show-toplevel)
+
+"${repo_root}/sysroot/build.sh" x86_64 "${repo_root}/sysroot" base
+"${repo_root}/sysroot/build.sh" armv7 "${repo_root}/sysroot" base
+"${repo_root}/sysroot/build.sh" aarch64 "${repo_root}/sysroot" base
+
+"${repo_root}/sysroot/build.sh" x86_64 "${repo_root}/sysroot" X11
